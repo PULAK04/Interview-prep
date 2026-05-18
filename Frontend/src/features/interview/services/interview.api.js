ba077528acfd2a48de938267af04e7ecef419620
Shayan Adhikary <shayanadhikary25@gmail.com>
2026-05-18 17:27:26 +0530

    Update interview.api.js

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interview-prep-9tlp.onrender.com",
     withCredentials: true,
 })
 
