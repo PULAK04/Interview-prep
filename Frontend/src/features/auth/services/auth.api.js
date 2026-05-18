39e132896615e1b52c3ae80c714c3a35c8d30ce0
Shayan Adhikary <shayanadhikary25@gmail.com>
2026-05-18 17:42:59 +0530

    Update auth.api.js

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interview-prep-9tlp.onrender.com",
     withCredentials: true
 })
 
